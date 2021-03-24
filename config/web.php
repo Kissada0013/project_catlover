70c1c261f60d34911cf43ca39a9dee822c4fd651
kissada_laha <kissada.lah@msu.ac.th>
2021-03-24 11:52:10 +0700

    make it better

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'controllerNamespace' => 'cat\\controllers',
     'defaultRoute' => 'cat/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
